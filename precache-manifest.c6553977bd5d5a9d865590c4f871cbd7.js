788150e488ba1f788a6527536ffbbebc7b2596ef
Ivomar Brito Soares <ivomarbsoares@gmail.com>
2020-09-04 17:29:15 -0300

    Updates

---
+++ precache-manifest.c6553977bd5d5a9d865590c4f871cbd7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f26a93527cb0bdd8c25c1a803a2652d",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/home/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5f3fea680e5bb085f725",
+    "url": "/home/static/css/main.9fc689ea.chunk.css"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/home/static/js/2.8fa335fd.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/home/static/js/2.8fa335fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f3fea680e5bb085f725",
+    "url": "/home/static/js/main.16eb8057.chunk.js"
+  },
+  {
+    "revision": "5c07ecca0456e28e04f0",
+    "url": "/home/static/js/runtime-main.e084aacb.js"
+  },
+  {
+    "revision": "6ca30490cbacab1e89c55c799b83ae32",
+    "url": "/home/static/media/resume.6ca30490.pdf"
+  }
+]);
